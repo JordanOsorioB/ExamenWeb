5e8b78c8301a467b9f6be9f2ba66e4ca3a7c2ac3
Jordán Osorio B. <jordanjesusosoriobustos@gmail.com>
2024-07-08 20:38:42 -0400

    Avance index

--- examenWeb/biblioteca/models.py
+++ examenWeb/biblioteca/models.py
@@ -3,24 +3,25 @@ from django.db import models
 # Create your models here.
 class Autor(models.Model):
 			id_autor      = models.AutoField(db_column="idAutor", primary_key=True)
-			nombre        = models.CharField(max_length=25, blank=False, null=False)
+			nombre        = models.CharField(max_length=40, blank=False, null=False)
 			nacionalidad  = models.CharField(max_length=20, blank=False, null=False)
+			descripcion	  = models.CharField(max_length=500)
 			foto_autor    = models.CharField(max_length=100)
 
 class Libro(models.Model):
 			id_libro          = models.AutoField(db_column="idLibro", primary_key=True)
-			titulo            = models.CharField( max_length=20)
+			titulo            = models.CharField( max_length=50)
 			año_publicacion   = models.DateField(blank=False, null=False) 
-			descripcion_breve = models.CharField(max_length=50)
+			descripcion_breve = models.CharField(max_length=500,default='Sin descripción', blank=True, null=False)
 			portada           = models.CharField(max_length=100,null=True, blank=True)
 			
 class Categoria(models.Model):
 			id_categoria      = models.AutoField(db_column="idCategoria", primary_key=True)
-			nombre_categoria  = models.CharField(max_length=25, blank=False, null=False)
-			descripcion       = models.CharField(max_length=100)
+			nombre_categoria  = models.CharField(max_length=35, blank=False, null=False)
+			foto_categoria    = models.CharField(max_length=100)
 
 class NavItem(models.Model):
 			id_navItem  = models.AutoField(db_column="idNavItem", primary_key=True)
-			titulo      = models.CharField(max_length=20)
+			titulo      = models.CharField(max_length=35)
 			url         = models.CharField(max_length=100,null=True)
 
