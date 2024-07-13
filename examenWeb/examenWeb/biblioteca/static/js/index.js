93e8eb4a716453d835c2ebc4c948f9cda36c98b1
Jordán Osorio B. <jordanjesusosoriobustos@gmail.com>
2024-07-12 21:33:43 -0400

    Ultimas modificaciones

--- examenWeb/examenWeb/biblioteca/static/js/index.js
+++ examenWeb/examenWeb/biblioteca/static/js/index.js
@@ -1,7 +1,7 @@
 AOS.init();
 
-$("#searchInput").on("input", function () {
-  const query = $(this).val().toLowerCase();
+document.getElementById('searchInput').addEventListener('input', function() {
+  const query = this.value.toLowerCase();
   const $cards = $(".libro-card");
   const $librosTitulo = $("#librosTitulo");
   const $autoresTitulo = $("#autoresTitulo");
@@ -9,56 +9,58 @@ $("#searchInput").on("input", function () {
 
   let hasResults = false;
 
-  $cards.each(function () {
-    const $card = $(this);
-    const titulo = $card.find(".card-title").text().toLowerCase();
-    const autor = $card.find(".card-autor").length ? $card.find(".card-autor").text().toLowerCase() : "";
-    const categoria = $card.find(".card-categoria").length ? $card.find(".card-categoria").text().toLowerCase() : "";
+  // Desactivar AOS para que no interfiere en la busqueda.
+  AOS.init({
+      disable: true
+  });
 
-    if (titulo.includes(query) || autor.includes(query) || categoria.includes(query)) {
-      $card.show();
-      hasResults = true;
-    } else {
-      $card.hide();
-    }
+  $cards.each(function () {
+      const $card = $(this);
+      const titulo = $card.find(".card-title").text().toLowerCase();
+      const autor = $card.find(".card-autor").length ? $card.find(".card-autor").text().toLowerCase() : "";
+      const categoria = $card.find(".card-categoria").length ? $card.find(".card-categoria").text().toLowerCase() : "";
+
+      if (titulo.includes(query) || autor.includes(query) || categoria.includes(query)) {
+          $card.show();
+          hasResults = true;
+      } else {
+          $card.hide();
+      }
   });
 
   if (query && hasResults) {
-    $librosTitulo.hide();
-    $autoresTitulo.hide();
-    $categoriasTitulo.hide();
+      $librosTitulo.hide();
+      $autoresTitulo.hide();
+      $categoriasTitulo.hide();
   } else if (query && !hasResults) {
-    Swal.fire({
-      template: "#noResults",
-    });
+      Swal.fire({
+          template: "#noResults",
+      });
   } else {
-    $librosTitulo.show();
-    $autoresTitulo.show();
-    $categoriasTitulo.show();
+      $librosTitulo.show();
+      $autoresTitulo.show();
+      $categoriasTitulo.show();
   }
+
 });
 
+/* Aqui hacemos la logica para desactivar o activar ciertas lineas dependiendo si queremos ver mas o menos */
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Obtener todos los botones "Ver más..." y "Ver menos..."
   const verMasBtns = document.querySelectorAll(
     ".buscarLibroBtn, .buscarAutorBtn"
   );
   const verMenosBtns = document.querySelectorAll(".verMenos");
 
-  // Añadir un evento de clic a cada botón "Ver más..."
   verMasBtns.forEach((btn) => {
     btn.addEventListener("click", function (event) {
-      event.preventDefault(); // Evitar que el enlace siga su href
+      event.preventDefault(); 
 
-      // Obtener la tarjeta padre del botón
       const cardBody = this.closest(".card-body");
 
-      // Obtener el elemento de detalles dentro de la tarjeta
       const detalles = cardBody.querySelector(".detalles");
       const verMenosBtn = cardBody.querySelector(".verMenos");
 
-      // Cambiar el estado de display de 'none' a 'block'
       if (detalles) {
         detalles.style.display = "block";
         this.style.display = "none";
@@ -69,21 +71,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Añadir un evento de clic a cada botón "Ver menos..."
   verMenosBtns.forEach((btn) => {
     btn.addEventListener("click", function (event) {
-      event.preventDefault(); // Evitar que el enlace siga su href
+      event.preventDefault(); 
 
-      // Obtener la tarjeta padre del botón
+
       const cardBody = this.closest(".card-body");
 
-      // Obtener el elemento de detalles dentro de la tarjeta
       const detalles = cardBody.querySelector(".detalles");
       const verMasBtn = cardBody.querySelector(
         ".buscarLibroBtn, .buscarAutorBtn"
       );
 
-      // Cambiar el estado de display de 'block' a 'none'
+
       if (detalles) {
         detalles.style.display = "none";
         this.style.display = "none";
--- examenWeb/examenWeb/biblioteca/urls.py
+++ examenWeb/examenWeb/biblioteca/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-			path('index', views.index, name="index"),
+			path('', views.index, name="index"),
 			path('categoria', views.categoria, name="categoria"),
             path('libros', views.libros, name="libros"),
 			path('autor', views.autor, name="autor")
